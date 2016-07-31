d5c81bf8668f5a19e8fc174038417e44b9c76bc6
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-31 17:18:54 +0100

    going round in circles with trying to clear the screen

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -234,42 +234,29 @@
 	   inboundCarrier: ""
 	}
 	
-	// var options = []
-	// var option1 = 0
-	// var option2 = 0
-	// var outboundCarrier = ""
-	// var inboundCarrier = ""
-	
 	var DisplayFlights = function( savedFlight ) {
-	
 	
 	  savedFlight.Quotes.forEach( function( flight, index ) {
 	    if( flight.Direct === true && flight.OutboundLeg != undefined && flight.InboundLeg != undefined ) {
 	      state.option1 = {
 	        cost: flight.MinPrice,
 	        outboundCarrierId: flight.OutboundLeg.CarrierIds[0],
-	        inboundCarrierId: flight.InboundLeg.CarrierIds[0]
-	        
+	        inboundCarrierId: flight.InboundLeg.CarrierIds[0]      
 	      }    
-	
-	      console.log( state.option1 )
 	    } else {
 	      if( flight.Direct === true ) {
 	        state.option2 += flight.MinPrice
-	        console.log( state.option2 )
 	      } else {
 	        console.log( "not direct buddy")
 	      }
 	    }
 	  })
 	
-	
 	  outboundName = function( ){
 	    savedFlight.Carriers.forEach(function( carrier, index){
 	      if(state.option1.outboundCarrierId === carrier.CarrierId){
 	      state.outboundCarrier = carrier.Name  }
 	    })
-	    
 	  }
 	
 	  inboundName = function(){
@@ -281,10 +268,7 @@
 	
 	  outboundName()
 	  inboundName()
-	  console.log(state.outboundCarrier)
-	  console.log(state.inboundCarrier)
 	  
-	
 	  if( state.option1 != 0 && state.option1 != undefined ) {
 	   state.options.push( state.option1 )
 	  }
@@ -293,20 +277,19 @@
 	   state.options.push( state.option2 )
 	  }
 	
-	
 	  var sortedOptions = state.options.sort( function( a, b ) {
-	    return a - b
+	    var answer = ( a - b )
+	    return answer
 	  })
 	
-	  console.log( sortedOptions )
-	
-	    display( savedFlight, sortedOptions )
-	  }
+	  display( savedFlight, sortedOptions )
+	}
 	
 	var display = function( savedFlight, options ) {
 	  var flight = document.getElementById( 'flight' );
 	  flight.innerHTML = ""
 	  state.options.forEach( function( option, index) {
+	
 	    var cost = document.createElement( 'p' );
 	    var outbound = document.createElement( 'p' );
 	    var inbound = document.createElement( 'p' );
@@ -317,100 +300,11 @@
 	    flight.appendChild( outbound )
 	    flight.appendChild( inbound )
 	  })
-	
+	   console.log( state.options )
+	   state.options.splice( 0, state.options.length )
 	}
 	module.exports = DisplayFlights;
-	
-	// var DisplayFlights = function( savedFlight ) {
-	
-	//   var options = []
-	//   var option1 = 0
-	//   var option2 = 0
-	//   var outboundCarrier = ""
-	//   var inboundCarrier = ""
-	  
-	
-	
-	
-	//   savedFlight.Quotes.forEach( function( flight, index ) {
-	//     if( flight.Direct === true && flight.OutboundLeg != undefined && flight.InboundLeg != undefined ) {
-	//       option1 = {
-	//         cost: flight.MinPrice,
-	//         outboundCarrierId: flight.OutboundLeg.CarrierIds[0],
-	//         inboundCarrierId: flight.InboundLeg.CarrierIds[0]
-	        
-	//       }    
-	
-	//       console.log( option1 )
-	//     } else {
-	//       if( flight.Direct === true ) {
-	//         option2 += flight.MinPrice
-	//         console.log( option2 )
-	//       } else {
-	//         console.log( "not direct buddy")
-	//       }
-	//     }
-	//   })
-	
-	
-	//   outboundName = function( ){
-	//     savedFlight.Carriers.forEach(function( carrier, index){
-	//       if(option1.outboundCarrierId === carrier.CarrierId){
-	//       outboundCarrier = carrier.Name  }
-	//     })
-	    
-	//   }
-	
-	//   inboundName = function(){
-	//     savedFlight.Carriers.forEach(function( carrier, index){
-	//       if(option1.inboundCarrierId === carrier.CarrierId){
-	//       inboundCarrier = carrier.Name  }
-	//       })
-	//   }
-	
-	//   outboundName()
-	//   inboundName()
-	//   console.log(outboundCarrier)
-	//   console.log(inboundCarrier)
-	  
-	
-	// if( option1 != 0 && option1 != undefined ) {
-	//   options.push( option1 )
-	// }
-	
-	// if( option2 != 0 && option2 != undefined ) {
-	//   options.push( option2 )
-	// }
-	
-	
-	// var sortedOptions = options.sort( function( a, b ) {
-	//   return a - b
-	// })
-	
-	// console.log( sortedOptions )
-	
-	//   display( savedFlight, sortedOptions )
-	// }
-	
-	// var display = function( savedFlight, options ) {
-	//   outboundName()
-	//   inboundName()
-	//   var flight = document.getElementById( 'flight' );
-	//   flight.innerHTML = ""
-	//   options.forEach( function( option, index) {
-	//     var cost = document.createElement( 'p' );
-	//     var outbound = document.createElement( 'p' );
-	//     var inbound = document.createElement( 'p' );
-	//     cost.innerHTML = "Cost: £" + option.cost
-	//     outbound.innerHTML = "Outbound Carrier: " + outboundCarrier 
-	//     inbound.innerHTML = "Inbound Carrier: " + inboundCarrier 
-	//     flight.appendChild( cost )
-	//     flight.appendChild( outbound )
-	//     flight.appendChild( inbound )
-	//   })
-	
-	// }
-	// module.exports = DisplayFlights;
+
 
 /***/ },
 /* 2 */
--- client/src/views/flightViewer.js
+++ client/src/views/flightViewer.js
@@ -6,42 +6,29 @@ var state = {
    inboundCarrier: ""
 }
 
-// var options = []
-// var option1 = 0
-// var option2 = 0
-// var outboundCarrier = ""
-// var inboundCarrier = ""
-
 var DisplayFlights = function( savedFlight ) {
-
 
   savedFlight.Quotes.forEach( function( flight, index ) {
     if( flight.Direct === true && flight.OutboundLeg != undefined && flight.InboundLeg != undefined ) {
       state.option1 = {
         cost: flight.MinPrice,
         outboundCarrierId: flight.OutboundLeg.CarrierIds[0],
-        inboundCarrierId: flight.InboundLeg.CarrierIds[0]
-        
+        inboundCarrierId: flight.InboundLeg.CarrierIds[0]      
       }    
-
-      console.log( state.option1 )
     } else {
       if( flight.Direct === true ) {
         state.option2 += flight.MinPrice
-        console.log( state.option2 )
       } else {
         console.log( "not direct buddy")
       }
     }
   })
 
-
   outboundName = function( ){
     savedFlight.Carriers.forEach(function( carrier, index){
       if(state.option1.outboundCarrierId === carrier.CarrierId){
       state.outboundCarrier = carrier.Name  }
     })
-    
   }
 
   inboundName = function(){
@@ -53,10 +40,7 @@ var DisplayFlights = function( savedFlight ) {
 
   outboundName()
   inboundName()
-  console.log(state.outboundCarrier)
-  console.log(state.inboundCarrier)
   
-
   if( state.option1 != 0 && state.option1 != undefined ) {
    state.options.push( state.option1 )
   }
@@ -65,20 +49,19 @@ var DisplayFlights = function( savedFlight ) {
    state.options.push( state.option2 )
   }
 
-
   var sortedOptions = state.options.sort( function( a, b ) {
-    return a - b
+    var answer = ( a - b )
+    return answer
   })
 
-  console.log( sortedOptions )
-
-    display( savedFlight, sortedOptions )
-  }
+  display( savedFlight, sortedOptions )
+}
 
 var display = function( savedFlight, options ) {
   var flight = document.getElementById( 'flight' );
   flight.innerHTML = ""
   state.options.forEach( function( option, index) {
+
     var cost = document.createElement( 'p' );
     var outbound = document.createElement( 'p' );
     var inbound = document.createElement( 'p' );
@@ -89,97 +72,7 @@ var display = function( savedFlight, options ) {
     flight.appendChild( outbound )
     flight.appendChild( inbound )
   })
-
+   console.log( state.options )
+   state.options.splice( 0, state.options.length )
 }
 module.exports = DisplayFlights;
-
-// var DisplayFlights = function( savedFlight ) {
-
-//   var options = []
-//   var option1 = 0
-//   var option2 = 0
-//   var outboundCarrier = ""
-//   var inboundCarrier = ""
-  
-
-
-
-//   savedFlight.Quotes.forEach( function( flight, index ) {
-//     if( flight.Direct === true && flight.OutboundLeg != undefined && flight.InboundLeg != undefined ) {
-//       option1 = {
-//         cost: flight.MinPrice,
-//         outboundCarrierId: flight.OutboundLeg.CarrierIds[0],
-//         inboundCarrierId: flight.InboundLeg.CarrierIds[0]
-        
-//       }    
-
-//       console.log( option1 )
-//     } else {
-//       if( flight.Direct === true ) {
-//         option2 += flight.MinPrice
-//         console.log( option2 )
-//       } else {
-//         console.log( "not direct buddy")
-//       }
-//     }
-//   })
-
-
-//   outboundName = function( ){
-//     savedFlight.Carriers.forEach(function( carrier, index){
-//       if(option1.outboundCarrierId === carrier.CarrierId){
-//       outboundCarrier = carrier.Name  }
-//     })
-    
-//   }
-
-//   inboundName = function(){
-//     savedFlight.Carriers.forEach(function( carrier, index){
-//       if(option1.inboundCarrierId === carrier.CarrierId){
-//       inboundCarrier = carrier.Name  }
-//       })
-//   }
-
-//   outboundName()
-//   inboundName()
-//   console.log(outboundCarrier)
-//   console.log(inboundCarrier)
-  
-
-// if( option1 != 0 && option1 != undefined ) {
-//   options.push( option1 )
-// }
-
-// if( option2 != 0 && option2 != undefined ) {
-//   options.push( option2 )
-// }
-
-
-// var sortedOptions = options.sort( function( a, b ) {
-//   return a - b
-// })
-
-// console.log( sortedOptions )
-
-//   display( savedFlight, sortedOptions )
-// }
-
-// var display = function( savedFlight, options ) {
-//   outboundName()
-//   inboundName()
-//   var flight = document.getElementById( 'flight' );
-//   flight.innerHTML = ""
-//   options.forEach( function( option, index) {
-//     var cost = document.createElement( 'p' );
-//     var outbound = document.createElement( 'p' );
-//     var inbound = document.createElement( 'p' );
-//     cost.innerHTML = "Cost: £" + option.cost
-//     outbound.innerHTML = "Outbound Carrier: " + outboundCarrier 
-//     inbound.innerHTML = "Inbound Carrier: " + inboundCarrier 
-//     flight.appendChild( cost )
-//     flight.appendChild( outbound )
-//     flight.appendChild( inbound )
-//   })
-
-// }
-// module.exports = DisplayFlights;
