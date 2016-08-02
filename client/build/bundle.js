d2e755724298546aa26858e3c36608cc461c400c
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-08-02 16:11:29 +0100

    started a login feature

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -53,7 +53,6 @@
 	var code;
 	
 	
-	
 	var capitalize = function( string ) {
 	  return string.charAt(0).toUpperCase() + string.slice(1);
 	}
@@ -61,6 +60,12 @@
 	
 	
 	window.onload = function(){
+	  var signIn = document.getElementById( 'signIn' )
+	  var showForm = document.getElementById( 'container' )
+	  signIn.onclick = function(e) {
+	    console.log( "clicked" )
+	    showForm.style.display = "block"
+	  }
 	  state = new State()
 	  display( 'people_slider', state.people )
 	  display( 'nights', state.nights )
@@ -328,6 +333,89 @@
 	
 	
 	}
+	
+	var Form = function() {
+	
+	  function Submit(){
+	   var emailRegex = /^[A-Za-z0-9._]*\@[A-Za-z]*\.[A-Za-z]{2,5}$/;
+	   var fname = document.form.Name.value,
+	    lname = document.form.LastName.value,
+	    femail = document.form.Email.value,
+	    freemail = document.form.enterEmail.value,
+	    fpassword = document.form.Password.value,
+	    freepassword = document.form.enterPassword.value
+	     
+	   if( fname == "" )
+	     {
+	       document.form.Name.focus() ;
+	    document.getElementById("errorBox").innerHTML = "enter the first name";
+	       return false;
+	     }
+	   if( lname == "" )
+	     {
+	       document.form.LastName.focus() ;
+	     document.getElementById("errorBox").innerHTML = "enter the last name";
+	       return false;
+	     }
+	      
+	     if (femail == "" )
+	   {
+	    document.form.Email.focus();
+	    document.getElementById("errorBox").innerHTML = "enter the email";
+	    return false;
+	    }else if(!emailRegex.test(femail)){
+	    document.form.Email.focus();
+	    document.getElementById("errorBox").innerHTML = "enter the valid email";
+	    return false;
+	    }
+	     
+	     if (freemail == "" )
+	   {
+	    document.form.enterEmail.focus();
+	    document.getElementById("errorBox").innerHTML = "Re-enter the email";
+	    return false;
+	    }else if(!emailRegex.test(freemail)){
+	    document.form.enterEmail.focus();
+	    document.getElementById("errorBox").innerHTML = "Re-enter the valid email";
+	    return false;
+	    }
+	     
+	    if(freemail !=  femail){
+	     document.form.enterEmail.focus();
+	     document.getElementById("errorBox").innerHTML = "emails are not matching, re-enter again";
+	     return false;
+	     }
+	     
+	     
+	   if(fpassword == "")
+	    {
+	     document.form.Password.focus();
+	     document.getElementById("errorBox").innerHTML = "enter the password";
+	     return false;
+	    }
+	
+	    if(freepassword == "")
+	     {
+	      document.form.Password.focus();
+	      document.getElementById("errorBox").innerHTML = "enter the password";
+	      return false;
+	     }
+	     
+	    if(freepassword !=  fepassword){
+	     document.form.enterEmail.focus();
+	     document.getElementById("errorBox").innerHTML = "passwords are not matching, re-enter again";
+	     return false;
+	     } 
+	
+	    if(fname != '' && lname != '' && femail != '' && freemail != '' && fpassword != '' ){
+	     document.getElementById("errorBox").innerHTML = "form submitted successfully";
+	     }
+	
+	
+	
+	       
+	  }
+	} 
 	
 	
 	
