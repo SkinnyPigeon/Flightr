5efa8a924147bd213482d06b4c4a60eef6cbf63f
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-08-03 09:14:13 +0100

    switched to main directory

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/', function (req, res) {
  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
+app.get('/user', function( req, res ) {
+  res.sendFile( path.join( __dirname + '/client/src/models/user.js' ))
+})
+
+
 app.use(express.static('client/build'));
 
 
@@ -37,9 +42,40 @@ app.get("/airports/:code", function(req,res){
  })
 })
 
+var url = 'mongodb://localhost:27017/users'
 
-// app.get ('/flights')
-//app.get ('/hotels')
-//app.get ('/uber')
+app.get('/users', function(req, res){
+ MongoClient.connect(url, function( err, db ){
+   var collection = db.collection('user');
+   collection.find({}).toArray( function(err, docs){
+    console.log( "Hiajshsjshajahsjahsjah")
+     res.json(docs);
+     db.close(); 
+   })
+ })
+})
+
+app.post('/users', function(req, res){
+ 
+ // res.status(200).end();
+ MongoClient.connect(userUrl, function( err, db ){
+ var collection = db.collection('user');
+ console.log( "HELAJSJDLKAKHSDKJAHSKJDHK" )
+ console.log( req.body )
+ collection.insert( req.body )
+ res.status(200).end();
+ db.close()
+})
+})
+
+ app.put('/users/:id', function(req, res){
+  MongoClient.connect( userUrl, function(err, db){
+    var collection = db.collection('users');
+    collection.update( {_id: new ObjectID(req.params.id)}, {$set:req.body} )
+    res.status(200).end();
+    db.close();
+  })
+ })
+
 
 // here we are not creating or updating we are just retrieving info
